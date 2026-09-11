189166dfeae74f37a8e0c5b73b02e2693358f1f3
Thiago <thifabris@gmail.com>
2026-09-10 21:17:45 -0300

    correção uri

--- src/main/java/com/estudo/agendadortarefas/infrastructure/client/UsuarioClient.java
+++ src/main/java/com/estudo/agendadortarefas/infrastructure/client/UsuarioClient.java
@@ -2,7 +2,6 @@ package com.estudo.agendadortarefas.infrastructure.client;
 
 import com.estudo.agendadortarefas.business.dto.UsuarioDTO;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @FeignClient(name = "usuario", url = "${usuario.url}")
 public interface UsuarioClient {
 
-    @GetMapping
+    @GetMapping("/usuario")
     UsuarioDTO buscaUsuarioPorEmail(@RequestHeader ("Authorization") String token, @RequestParam("email") String email);
 
 }
